f28950c0be1999d4214789910c78a01c3f2a3074
Thai Huu <huuthai37@gmail.com>
2018-03-25 23:19:32 +0700

    Change

--- ucf11/mobilenet_spatial.py
+++ ucf11/mobilenet_spatial.py
@@ -4,18 +4,15 @@ from keras.models import Model
 from keras.layers import Dense, Conv2D, Activation, Reshape, Flatten
 from keras import optimizers
 from keras.preprocessing.image import ImageDataGenerator
-from keras.callbacks import ModelCheckpoint
+from keras.callbacks import ModelCheckpoint, Callback
 import pickle
 import random
 import config
 
-# from matplotlib import pyplot as plt
-# from IPython.display import clear_output
-
 # train: python mobilenet_spatial.py train 32 1 101
 # test: python mobilenet_spatial.py test 32 1 101
 # retrain: python mobilenet_spatial.py retrain 32 1 101 1
-class PlotLearning(keras.callbacks.Callback):
+class PlotLearning(Callback):
     def on_train_begin(self, logs={}):
         # self.i = 0
         self.logs = []
@@ -47,15 +44,23 @@ classes = int(sys.argv[4])
 server = config.server()
 if server:
     train_path = '/home/oanhnt/thainh/data/rgb/train'
+    valid_path = '/home/oanhnt/thainh/data/rgb/valid'
     test_path = '/home/oanhnt/thainh/data/rgb/test'
 else:
     train_path = '/mnt/data11/rgb/train'
+    valid_path = '/mnt/data11/rgb/valid'
     test_path = '/mnt/data11/rgb/test'
 
 if train:
     len_samples = 86908
     train_batches = ImageDataGenerator(rescale=1./255).flow_from_directory(
         train_path,
+        batch_size=batch_size,
+        target_size=(224, 224),
+    )
+    len_valid = 2000
+    valid_batches = ImageDataGenerator(rescale=1./255).flow_from_directory(
+        valid_path,
         batch_size=batch_size,
         target_size=(224, 224),
     )
@@ -71,11 +76,13 @@ else:
 if train & (not retrain):
     model = keras.applications.mobilenet.MobileNet(
         include_top=True,  
-        weights='imagenet'
+        weights='imagenet',
+        dropout=0.5
     )
 else:
     model = keras.applications.mobilenet.MobileNet(
         include_top=True,  
+        dropout=0.5
     )
 
 # Modify network some last layer
@@ -101,8 +108,16 @@ if train:
     print('MobileNet RGB stream only: Training')
     print('-'*40)
     check_point = ModelCheckpoint('weights/mobilenet_spatial_{epoch}e.h5', verbose=1, save_weights_only=True)
-    result_model.fit_generator(train_batches, verbose=1, callbacks=[plot_losses,check_point], max_queue_size=2, steps_per_epoch=len_samples/batch_size, epochs=epochs)
-    # result_model.save_weights('weights/mobilenet_spatial_{}e.h5'.format(old_epochs+1+e))
+    result_model.fit_generator(
+        train_batches, 
+        verbose=1, 
+        callbacks=[plot_losses,check_point], 
+        max_queue_size=2, 
+        steps_per_epoch=len_samples/batch_size, 
+        epochs=epochs,
+        validation_data=valid_batches,
+        validation_steps=len_valid/batch_size)
+
 else:
     result_model.load_weights('weights/mobilenet_spatial_{}e.h5'.format(epochs))
 
